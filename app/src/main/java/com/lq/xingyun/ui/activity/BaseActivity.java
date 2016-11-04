a821e6b19d2acc410957390a11ec3839d80da6f7
liqiao1992 <393347970@qq.com>
2016-11-04 17:18:50 +0800

    全屏播放页面

--- app/src/main/java/com/lq/xingyun/ui/activity/BaseActivity.java
+++ app/src/main/java/com/lq/xingyun/ui/activity/BaseActivity.java
@@ -1,9 +1,15 @@
 package com.lq.xingyun.ui.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +30,8 @@ import com.lq.xingyun.utils.ActivityManagerUtil;
 import com.lq.xingyun.utils.SharedPreferencesUtils;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
+import java.util.Locale;
+
 /**
  * Created by lenovo on 2016/7/28.
  */
@@ -40,11 +48,12 @@ public abstract class BaseActivity extends AppCompatActivity implements IBase {
     protected void onCreate(Bundle savedInstanceState) {
         ActivityManagerUtil.addActivity(this);
         initTheme();
+        changeLanguage();
         super.onCreate(savedInstanceState);
-        mIsAddedView=false;
-        if (SharedPreferencesUtils.getBoolean(getApplicationContext(), "isNight")){
-            initNightView();
-            mNightView.setBackgroundResource(R.color.night_mask);
+        mIsAddedView = false;
+        if (SharedPreferencesUtils.getBoolean("night_mode")) {
+//            initNightView();
+//            mNightView.setBackgroundResource(R.color.night_mask);
         }
         mPresenter = getPresenter();
         if (mPresenter != null && this instanceof IBaseView) {
@@ -60,38 +69,104 @@ public abstract class BaseActivity extends AppCompatActivity implements IBase {
         setActionBar();
 
     }
-
+    protected void changeLanguage(){
+        int languageIndex=SharedPreferencesUtils.getInteger("language_mode");
+        if(languageIndex>-1) {
+            String language = null;
+            String country = null;
+            switch (languageIndex) {
+                case 0:
+                    language = "en";
+                    country = "US";
+                    break;
+                case 1:
+                    language = "zh";
+                    country = "CN";
+                    break;
+                case 2:
+                    language = "zh";
+                    country = "TW";
+                    break;
+            }
+            Locale locale = new Locale(language, country);
+            Configuration configuration = getResources().getConfiguration();
+            configuration.locale = locale;
+            getResources().updateConfiguration(configuration, getResources().getDisplayMetrics());
+        }
+    }
     public void initTheme() {
-        if (SharedPreferencesUtils.getBoolean(getApplicationContext(), "isNight")) {
+        if (SharedPreferencesUtils.getBoolean( "night_mode")) {
             setTheme(R.style.AppTheme_night);
         } else {
             setTheme(R.style.AppTheme_day);
         }
 
     }
+
     protected void ChangeToDay() {
-        SharedPreferencesUtils.save(getApplicationContext(),"isNight",false);
-        if(mIsAddedView&&mNightView!=null) {
-//            mNightView.setBackgroundResource(android.R.color.transparent);
+//        showAnimation();
+        if (mIsAddedView && mNightView != null) {
             mWindowManager.removeViewImmediate(mNightView);
         }
     }
 
     protected void ChangeToNight() {
-        SharedPreferencesUtils.save(getApplicationContext(),"isNight",true);
+//        showAnimation();
         initNightView();
     }
+
     protected void initNightView() {
         if (mIsAddedView)
             return;
         WindowManager.LayoutParams mNightViewParam = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.TYPE_APPLICATION,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                 PixelFormat.TRANSPARENT);
         mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         mNightView = new View(this);
+        mNightView.setBackgroundResource(R.color.night_mask);
         mWindowManager.addView(mNightView, mNightViewParam);
         mIsAddedView = true;
+    }
+
+    public void showAnimation() {
+        Log.i("fuck","开始动画");
+        Bitmap cacheBitmap = getCaptureView();
+        if (getWindow().getDecorView() instanceof ViewGroup && cacheBitmap != null) {
+            Log.i("fuck","进入动画开始--------------------");
+            final View view = new View(this);
+            view.setBackgroundDrawable(new BitmapDrawable(getResources(), cacheBitmap));
+            ViewGroup.LayoutParams layoutParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            ((ViewGroup) getWindow().getDecorView()).addView(view, layoutParam);
+            ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f);
+            objectAnimator.setDuration(3000);
+            objectAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    ((ViewGroup) getWindow().getDecorView()).removeView(view);
+                }
+            });
+            objectAnimator.start();
+        }
+    }
+
+    private Bitmap getCaptureView() {
+        View view = getWindow().getDecorView();
+        final boolean drawingCacheEnabled = true;
+        view.setDrawingCacheEnabled(drawingCacheEnabled);
+        view.buildDrawingCache(drawingCacheEnabled);
+        final Bitmap drawingCache = view.getDrawingCache();
+        Bitmap bitmap;
+        if (drawingCache != null) {
+            bitmap = Bitmap.createBitmap(drawingCache);
+            view.setDrawingCacheEnabled(false);
+        } else {
+            bitmap = null;
+        }
+        view.destroyDrawingCache();
+        return bitmap;
     }
 
     public void getIntentValue() {
@@ -157,7 +232,7 @@ public abstract class BaseActivity extends AppCompatActivity implements IBase {
     @TargetApi(19)
     private void initWindow() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && isSetStatusBar()) {
-            Log.i("fuck","设置沉浸");
+            Log.i("fuck", "设置沉浸");
             getWindow().addFlags(
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
