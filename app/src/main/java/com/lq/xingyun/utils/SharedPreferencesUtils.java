bfb379d5a6d513ee64de01f92be7bd72d79590cb
liqiao1992 <393347970@qq.com>
2016-11-04 17:29:43 +0800

    全屏播放页面

--- app/src/main/java/com/lq/xingyun/utils/SharedPreferencesUtils.java
+++ app/src/main/java/com/lq/xingyun/utils/SharedPreferencesUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.lq.xingyun.XinYunApplication;
+
 /**
  * Created by lenovo on 2016/8/10.
  */
@@ -13,10 +15,18 @@ public class SharedPreferencesUtils {
            return PreferenceManager.getDefaultSharedPreferences(context);
     }
 
-    public static void save(Context context,String key,boolean value){
-        getSharedPreferences(context).edit().putBoolean(key,value).commit();
+    public static void save(String key,boolean value){
+        getSharedPreferences(XinYunApplication.getContext()).edit().putBoolean(key,value).commit();
     }
-    public static boolean getBoolean(Context context,String key){
-        return getSharedPreferences(context).getBoolean(key,false);
+    public static boolean getBoolean(String key){
+        return getSharedPreferences(XinYunApplication.getContext()).getBoolean(key,false);
+    }
+
+    public  static void save(String key,int value){
+        getSharedPreferences(XinYunApplication.getContext()).edit().putInt(key,value).commit();
+    }
+
+    public static int getInteger(String key){
+        return getSharedPreferences(XinYunApplication.getContext()).getInt(key,-1);
     }
 }
